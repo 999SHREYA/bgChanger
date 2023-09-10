3ababe74d954e4d9d98684a8b6836a5767371a5d
999SHREYA <mishrashreya900@gmail.com>
2023-09-10 17:21:57 +0530

    bgChanger

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,22 @@ function App() {
 const [color, setColor] = useState("black")
   return (
     <>
-      <div className="w-full h-screen" style={{ backgroundColor: color }}>
-        <div className="">
-          <div className="">
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center top-11 left-3/4 inset-x-0 px-2">
+          <div className="  gap-3 shadow-sm bg-white rounded-sm">
             <button onClick={() => setColor("red")}>Red</button>
+            <br />
             <button onClick={() => setColor("Green")}>Green</button>
+            <br />
             <button onClick={() => setColor("blue")}>blue</button>
+            <br />
             <button onClick={() => setColor("white")}>white</button>
+            <br />
             <button onClick={() => setColor("yellow")}>yellow</button>
+            <br />
           </div>
         </div>
       </div>
